157d97e7069d916ba7874801a5629748ed040bc6
Nick George <nicholas@bioteam.net>
2022-06-09 08:13:15 -0400

    Add new data files and updates

--- analysis/cfde-programs.R
+++ analysis/cfde-programs.R
@@ -4,7 +4,7 @@ library(ggplot2)
 library(jsonlite)
 theme_set(theme_bw())
 
-data <- bind_rows(purrr::map(list.files(path="data", pattern=".json",full.names = T), fromJSON))
+data <- bind_rows(purrr::map(list.files(path="data", pattern="-GEO.json",full.names = T), fromJSON))
 
 data %>%
   filter(program == c("Metabolomics",
@@ -13,5 +13,10 @@ data %>%
                       "Genotype-Tissue Expression (GTEx)")) %>%
   filter(publication_year > 2005) %>%
   ggplot(aes(x=publication_year, fill=program)) +
-  geom_bar(position=position_dodge2(preserve="single"), size=0.4,alpha=0.8) +
+  geom_bar(position=position_dodge2(preserve="single"),
+           size=0.4,alpha=0.8) +
   labs(x="Year", y="Publications Citing")
+
+
+
+
