26f407f59ba7e91d04ae5872624315eddc8694d5
Nick George <nicholas@bioteam.net>
2022-06-20 23:02:47 -0400

    updated flagship citation generator

--- commonfund/flagship.py
+++ commonfund/flagship.py
@@ -1,31 +1,23 @@
-# get citations from flagship publications
+# https://www.ncbi.nlm.nih.gov/pmc/tools/cites-citedby/
 import argparse
 import csv
 import json
-import os
 import sys
-import time
 
 
-import requests
+from commonfund import helper  # safe_request_json()
 
 
-def safe_request_json(url):
-    res = requests.get(url)
-    if res.ok:
-        return res.json()
-    # wait if rate limited
-    print(
-        f"Apparent rate limit for url {url}. Code: {res.status_code}. waiting 2 second then retry..."
+def create_cli(arguments=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description="Path to csv of flagship publications")
+    parser.add_argument(
+        "--flagship-key",
+        type=str,
+        required=True,
+        help="Path to JSON file containing the results",
+        dest="flagship_key",
     )
-    time.sleep(2)
-    res = requests.get(url)
-    try:
-        res.json()
-    except Exception as e:
-        print(f"[ERROR] Exception is {e}")
-        print(f"[ERROR] bad JSON returned from {res.url}")
-    return []
+    return parser.parse_args(arguments)
 
 
 def parse_csv(path):
@@ -37,87 +29,64 @@ def parse_csv(path):
     return out
 
 
-def parse_cited_by_entry(program, pmid, entry):
-    openalex = entry.get("id", "")
-    title = entry.get("title", "")
-    pmid_parsed = (
-        entry.get("ids", {})
-        .get("pmid", "")
-        .replace("https://pubmed.ncbi.nlm.nih.gov/", "")
-    )
-    published_date = entry.get("publication_date", "")
-    publication_year = entry.get("publication_year", None)
-    journal = entry.get("host_venue", {}).get("display_name", "")
-    total_citations = entry.get("cited_by_count", 0)
-    is_open_access = entry.get("open_access", {}).get("is_oa", None)
-    return {
-        "flagship_pmid": pmid,
-        "program": program,
-        "pmid": pmid_parsed,
-        "openalex": openalex,
-        "publication_title": title,
-        "published_date": published_date,
-        "publication_year": publication_year,
-        "journal": journal,
-        "total_citations": total_citations,
-        "is_open_access": is_open_access,
-    }
+def elink_api(entry):
+    INITIAL_DELAY = 0.1
+    out = {}
+    flagship_pmid = entry.get("PMID", None)
+    competes_with = entry.get("Competes with", "")
+    fund_type = entry.get("Funding", "")
+    fund_name = "unknown"
+    dcc_name = entry.get("DCC/Repo", None)
+    if fund_type == "CFDE":
+        fund_name = "cfde_dcc"
+    if fund_type == "External":
+        fund_name = "external"
+    if not fund_name in ["cfde_dcc", "external"]:
+        err_str = "[WARNING] Prog type not known, using 'unknown"
+        print(err_str)
+    if not competes_with:
+        competes_with = None
+    out["competes_with"] = competes_with
+    out["type"] = fund_name
+    out["program"] = dcc_name
+    out["flagship_pmid"] = flagship_pmid
+    if not flagship_pmid:
+        out["pmid_list"] = []
+        return out
+
+    base = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin&id={flagship_pmid}&retmode=json"
+    json_out = helper.safe_request_json(base, INITIAL_DELAY)
+    linksets = json_out.get("linksets")
+    try:
+        pmid_list = json_out.get("linksets")[0].get("linksetdbs")[0].get("links")
+    except Exception as e:
+        err_str = f"[WARNING] Problem getting PMIDs for {flagship_pmid}. Exception {e}"
+        print(err_str)
+        pmid_list = []
+    out["pmid_list"] = pmid_list
+    return out
 
 
-# journal, total_citations, is_open_access
-def open_alex_flagship(entry):
-    pmid = entry.get("PMID", None)
-    if not pmid:
-        return []
-    URL = f"https://api.openalex.org/works/pmid:{pmid}"
-    flagship_program = entry.get("DCC/Repo")
-    res_json = safe_request_json(URL)
-    if not res_json:
-        return []
-    cited_by_url = res_json.get("cited_by_api_url")
-    cited_by_json = safe_request_json(cited_by_url + "&per-page=200")
-    total = cited_by_json.get("meta", {}).get("count", 0)
-    if total == 0:
-        return []
-    cited_list = cited_by_json.get("results")
-    page = 1
-    while total > len(cited_list):
-        page += 1
-        url = f"{cited_by_url}&per-page=200&page={page}&mailto=nicholas@bioteam.net"
-        print(f"[INFO] Fetching URL:\n {url}")
-        cur_res = safe_request_json(url).get("results")
-        cited_list += cur_res
-
-    cited_data = []
-    for entry in cited_list:
-        cited_data.append(parse_cited_by_entry(flagship_program, pmid, entry))
-    return cited_data
-
-
-def write_out(entry, out):
-    n = 1
-    f_string = f"flagship-{entry.get('DCC/Repo')}-{n}.json"
-    out_path = os.path.join("data", f_string)
-    while os.path.exists(out_path):
-        n += 1
-        f_string = f"flagship-{entry.get('DCC/Repo')}-{n}.json"
-        out_path = os.path.join("data", f_string)
-    print(f"[INFO] Writing to path: {out_path}")
-    with open(out_path, "w") as f:
-        json.dump(out, f, indent=2)
-
+# testing
+# args = create_cli(["--flagship-key", "input/flagships.csv"])
+# parsed_csv = parse_csv(args.flagship_key)
+# new_name = "flagship_results.json"
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Path to CSV Key")
-    parser.add_argument("--key", help="path to flagship key")
-    args = parser.parse_args()
-    if not os.path.exists(args.key):
-        print("[ERROR] Please pass a valid path")
-        sys.exit()
-    csv_key = parse_csv(args.key)
-    for entry in csv_key:
-        print(f"Processing {entry['DCC/Repo']} PMID {entry['PMID']}")
-        out = open_alex_flagship(entry)
-        if not out:
+    args = create_cli()
+    parsed_csv = parse_csv(args.flagship_key)
+    len_total = len(parsed_csv)
+    new_name = "flagship_results.json"
+    flagships_out = []
+    for n, entry in enumerate(parsed_csv):
+        print(f"[INFO] Processing {n} out of {len_total}")
+        try:
+            temp = elink_api(entry)
+            flagships_out.append(temp)
+        except Exception as e:
+            err = f"Problem with index {n}, entry {entry}, Exception is {e}"
+            print(err)
             continue
-        write_out(entry, out)
+    with open(new_name, "w") as f:
+        json.dump(flagships_out, f)
+        print(f"Wrote results to {new_name}")
